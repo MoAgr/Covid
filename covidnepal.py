00856dc00f053ad7f1e5d8489905203572bb83aa
Aabhusan Aryal <aabhusanaryal@gmail.com>
2020-04-22 20:23:22 +0545

    Almost there boi

--- covidnepal.py
+++ covidnepal.py
@@ -3,12 +3,13 @@ import requests
 app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
-def cases:
+def cases():
     response=requests.get('https://api.covid19api.com/live/country/nepal/status/confirmed')
     json_object=response.json()
     confirmed_cases=int(json_object[-1]['Confirmed'])
     deaths=int(json_object[-1]['Deaths'])
     recovered=int(json_object[-1]['Recovered'])
     active=int(json_object[-1]['Active'])
+    return render_template("main.html", total = confirmed_cases, active = active, recovered = recovered)
     
 
