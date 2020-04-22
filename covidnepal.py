bd42050d93b5f41fed143549df8f84ca60eec1d7
Mohit Agarwal <mohit1999@live.in>
2020-04-22 19:36:03 +0545

    First Commit

---
+++ covidnepal.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,request
+import requests
+app=Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def cases:
+    response=requests.get('https://api.covid19api.com/live/country/nepal/status/confirmed')
+    json_object=response.json()
+    confirmed_cases=int(json_object[-1]['Confirmed'])
+    deaths=int(json_object[-1]['Deaths'])
+    recovered=int(json_object[-1]['Recovered'])
+    active=int(json_object[-1]['Active'])
+    
+
