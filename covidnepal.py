8331be6d68df53d0d4463916f341275ac3bd9c31
Mohit Agarwal <mohit1999@live.in>
2020-04-23 20:45:19 +0545

    Final Commit

--- covidnepal.py
+++ covidnepal.py
@@ -8,11 +8,23 @@ lockdown_end_date = date(2020, 4, 28)
 date_to_show = lockdown_end_date.strftime("%b %d")
 news_list = []
 t = tuple()
+news = requests.get(
+    'https://newsapi.org/v2/top-headlines?q=corona&apiKey=71026374f7a64e73896dfc7ef9c35d03&pageSize=50&page=2&language=en&from=' + str(
+        date.today() - timedelta(2)) + '&sortBy=popularityAt')
+news_json_obj = news.json()
 
+for i in range(0, 5):
+    title = str(news_json_obj['articles'][i]['title'])
+    source = str(news_json_obj['articles'][i]['source']['name'])
+    nlink = str(news_json_obj['articles'][i]['url'])
+    t = t + (title,)
+    t = t + (source,)
+    t = t + (nlink,)
+    news_list.append(t)
+    t = tuple()
 
 @app.route('/', methods=['GET', 'POST'])
 def cases():
-    global t
     response = requests.get('https://api.covid19api.com/live/country/nepal/status/confirmed')
     json_object = response.json()
     confirmed_cases = int(json_object[-1]['Confirmed'])
@@ -23,19 +35,8 @@ def cases():
     days_to_go = str(lockdown_end_date - todays_date).split(',')[0]
     time = str(datetime.now())
     time = time.split()[1].split(':')[0]
-    news = requests.get(
-        'https://newsapi.org/v2/top-headlines?q=corona&apiKey=71026374f7a64e73896dfc7ef9c35d03&pageSize=50&page=2&language=en&from=' + str(
-            date.today() - timedelta(2)) + '&sortBy=popularityAt')
-    news_json_obj = news.json()
-    for i in range(0, 5):
-        title = str(news_json_obj['articles'][i]['title'])
-        source = str(news_json_obj['articles'][i]['source']['name'])
-        nlink = str(news_json_obj['articles'][i]['url'])
-        t = t + (title,)
-        t = t + (source,)
-        t = t + (nlink,)
-        news_list.append(t)
-        t = tuple()
+
+
     return render_template("main.html", total=confirmed_cases, active=active, recovered=recovered, time=time,
                            lockdown_end_date=date_to_show, days_to_go=days_to_go, news_list=news_list)
 
